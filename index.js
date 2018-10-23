e1f82152698f8627a9126f54b005e39afcc4e8d6
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-23 15:40:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  };
+  else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+}
