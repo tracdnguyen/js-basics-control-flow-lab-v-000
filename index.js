6f2cf8930bf8d48a4587c853b2b43ddad1436a79
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-23 16:00:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  }
+   else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+}
